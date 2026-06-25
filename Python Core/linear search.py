4e9bcb2f5c7f568adab8b94262d1338ff741c276
SsmayankGupta <mgmayank2240@gmail.com>
2026-06-25 21:47:00 +0530

    Linear search

---
+++ Python Core/linear search.py
@@ -0,0 +1,8 @@
+
+lst=[1,21,0,23,1,22,3,4,5,3,2,45,3,3,4,55,90]
+tar=20
+
+if tar in lst:
+    print("Target element exists in the array.")
+else:
+    print("Target element does't exists in the array.")
